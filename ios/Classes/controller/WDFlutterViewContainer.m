aabf1bf777b84ea790b09aa1552309f9a186542d
chenzijie <chenzijie@weidian.com>
2019-07-01 15:59:10 +0800

    tab hiden

--- ios/Classes/controller/WDFlutterViewContainer.m
+++ ios/Classes/controller/WDFlutterViewContainer.m
@@ -150,12 +150,18 @@
             }
         });
     }
+    if (_routeOptions.isTab) {
+        self.tabBarController.tabBar.hidden = _flutterPageCount > 1;
+    }
 }
 
 - (void)flutterPageRemoved {
     _flutterPageCount --;
     if (_flutterPageCount <= 1) {
         self.navigationController.interactivePopGestureRecognizer.enabled = YES;
+    }
+    if (_routeOptions.isTab) {
+        self.tabBarController.tabBar.hidden = _flutterPageCount > 1;
     }
 }
 
