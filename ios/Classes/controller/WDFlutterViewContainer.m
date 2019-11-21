b89f52199def3c28646dafe12535bfc9314c465f
yangxin02 <yangxin02@weidian.com>
2019-11-21 17:08:57 +0800

    时间调大一点

--- ios/Classes/controller/WDFlutterViewContainer.m
+++ ios/Classes/controller/WDFlutterViewContainer.m
@@ -220,7 +220,7 @@
         [self.view bringSubviewToFront:self.fakeSnapImgView];
     }
 
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t) (0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t) (0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self.view bringSubviewToFront:FLUTTER_VIEWCONTROLLER_VIEW];
         self.lastSnapshot = nil;
     });
