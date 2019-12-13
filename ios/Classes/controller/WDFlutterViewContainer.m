a0f291cbed03c8c7a497b728a22ab335edaaf8d1
chenzijie <chenzijie@weidian.com>
2019-12-13 19:05:45 +0800

    fix present

--- ios/Classes/controller/WDFlutterViewContainer.m
+++ ios/Classes/controller/WDFlutterViewContainer.m
@@ -120,6 +120,10 @@ typedef void (^FlutterViewWillAppearBlock) (void);
 
 - (void)viewDidDisappear:(BOOL)animated {
     [super viewDidDisappear:animated];
+    //处理下present 页面卡死的情况
+    if ([WD_FLUTTER_ENGINE flutterViewController] != self) {
+        [[WD_FLUTTER_ENGINE flutterViewController] viewDidAppear:animated];
+    }
 }
 
 - (void)dealloc {
