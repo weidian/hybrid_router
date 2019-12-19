53e2df6abaaccf08e4051ac7fa3ad564d05aedcb
李樟取 <lizhangqu@weidian.com>
2019-12-19 12:17:33 +0800

    * update
    Change-Id: Iedf208a5e5b3619b8285bea8c78bda7ac9c042ed

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/qigengxin/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/qigengxin/Documents/work/vdian/flutter/repo_flutter_vdian/bundles/library/hybrid_router/example"
+export "FLUTTER_ROOT=/Users/lizhangqu/Library/Flutter/v1.12.13+hotfix.6/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/lizhangqu/AndroidStudioProjects/android-buyer/flutter_projects/buyer_repo/bundles/library/hybrid_router/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/qigengxin/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/lizhangqu/Library/Flutter/v1.12.13+hotfix.6/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
