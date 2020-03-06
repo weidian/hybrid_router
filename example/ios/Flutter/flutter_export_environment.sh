d6f2044b0e474a89aa2b095024ab3c922bae588b
chenzijie <chenzijie@weidian.com>
2020-03-06 13:48:54 +0800

    fix 1.12.13 fluter 返回 native 卡死 crash

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/qigengxin/fvm/versions/1.9.1+hotfix.6"
-export "FLUTTER_APPLICATION_PATH=/Users/qigengxin/Documents/Github/hybrid_router/example"
+export "FLUTTER_ROOT=/Users/blackox626/Library/Flutter/v1.12.13+hotfix.800002/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/blackox626/FlutterCode/hybrid_router/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/qigengxin/fvm/versions/1.9.1+hotfix.6/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/blackox626/Library/Flutter/v1.12.13+hotfix.800002/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
